3a07f7977a1c3a76414b89649c11c2af00a51d37
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-07-24 16:58:23 +0900

    Create project

---
+++ KudanARSample-iOS/KudanARSample-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KudanARSample-iOS
+//
+//  Created by Yoshito Tabuchi on 2018/07/24.
+//  Copyright © 2018年 Yoshito Tabuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
