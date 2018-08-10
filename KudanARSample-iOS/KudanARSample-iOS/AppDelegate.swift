02ea58db2730d99c2080b92b3a08f605b66bf6b6
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-08-10 19:09:22 +0900

    Add Trackable and ImageNode

--- KudanARSample-iOS/KudanARSample-iOS/AppDelegate.swift
+++ KudanARSample-iOS/KudanARSample-iOS/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import KudanAR
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Add APIKey
+        ARAPIKey.sharedInstance().setAPIKey("agWZcpYLYjBxCbWf2qZx6k+PWISqeGtFCqKaZwYtwS+kdn1HKiQAmsJ55STRBe9BqCw3VwG6qL+ESI5ntTF/iV/uekLG3PCokaUE0/uTzqhaYlxRdmuNBIduzBCjq3mV2na+gy3ffHH9Ipc7eIN0geTj3p+ppsmK0U399iGmN38ndIh6k2y16cByWIecMSU3yw3Ztw7gHRqf83hVhZ5T2ACGK4SNkQhhdKp+CTaR5W3amYCJBgwumqFqNFyI9UniuMk70T/cQObRQum2U51OjjbMfmEAwIBt8Q8jD2yACzye6K4/1O4pZhbGEbiDeLrAfxqMwBAe5o6vnYIilGNnpDhfi3wOHhRaqtLOVvB58GUIFTnAPvmYFVnLWRJmCUZ9FJNDyX3ALCl/alFEWh+A/a6NFjcwLGKI9drPuGG4ONFg4p0l+p3b9DZoLzszlmWAflI/UFzQa++kQn3/sclO9i0vPnpi0LWoABm5vGswLVAIX/0k6384GXxfkADI6fjGtf62XJ5ImaVDiiREa9mabWEQGoifghQG1sGNDYgBIYEpiaLsVzOfTALpe20Q7kFCMjedJImQhhuLtEK1BXfXJEed1QqUOsG9IeKxKk28GbOtOF9w3yrSF3gnJslzZxF2kEF3C6ckog8byagS+4p37FJmbpPsiKNH1Qm0LuouGcQ=")
+        
         return true
     }
 
--- KudanARSample-iOS/KudanARSample-iOS/ViewController.swift
+++ KudanARSample-iOS/KudanARSample-iOS/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import KudanAR
 
-class ViewController: UIViewController {
+class ViewController: ARCameraViewController {
 
+    override func setupContent() {
+        // Setup code goes here
+        // ARImageTrackable オブジェクトのインスタンス化と初期化
+        let imageTrackable = ARImageTrackable.init(image: UIImage(named: "Lego.jpg"), name: "Lego")
+        
+        // image tracker manager のインスタンスを取得して初期化
+        let imageTrackerManager = ARImageTrackerManager.getInstance()
+        imageTrackerManager?.initialise()
+        
+        // ARImageTrackable を　image tracker manager に追加
+        imageTrackerManager?.addTrackable(imageTrackable)
+        
+        // ImageNode を表示させたい画像で初期化
+        // PNG の場合は拡張子は不要です。
+        let imageNode = ARImageNode(image: UIImage(named: "Cow"))
+        
+        // ARImageTrackable に imageNode を追加
+        imageTrackable?.world.addChild(imageNode)
+
+        // マーカー画像のサイズに合わせるように、それぞれの幅から拡大率を計算
+        let scaleRatio = Float(imageTrackable!.width)/Float(imageNode!.texture.width)
+        
+        // 拡大率を ImageNode に適用
+        imageNode?.scale(byUniform: scaleRatio)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
