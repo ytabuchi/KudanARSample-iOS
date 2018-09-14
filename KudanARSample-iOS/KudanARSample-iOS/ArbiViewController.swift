f2f06e7114b6bb46242eb46b571e19de0e26817e
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2018-09-14 17:40:24 +0900

    Add Markerless View
    Modify storyboard. Change ViewContorller to MarkerViewController. And add ArbiViewController.

---
+++ KudanARSample-iOS/KudanARSample-iOS/ArbiViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ArbiViewController.swift
+//  KudanARSample-iOS
+//
+//  Created by Yoshito Tabuchi on 2018/09/14.
+//  Copyright © 2018年 Yoshito Tabuchi. All rights reserved.
+//
+
+import Foundation
+import KudanAR
+
+class ArbiViewController: ARCameraViewController {
+    
+}
