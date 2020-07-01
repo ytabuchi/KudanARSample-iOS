1b1bba3393bdab8a1cb79b72f480424eb07a940e
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2020-07-01 18:58:10 +0900

    Replace Markerless
    image to model

--- KudanARSample-iOS/KudanARSample-iOS/ArbiViewController.swift
+++ KudanARSample-iOS/KudanARSample-iOS/ArbiViewController.swift
@@ -11,7 +11,7 @@ import KudanAR
 
 class ArbiViewController: ARCameraViewController {
     
-    var trackingNode:ARImageNode?
+    var modelNode:ARModelNode?
     
     @IBOutlet weak var changeTrackingModeButton: UIButton!
     
@@ -36,20 +36,35 @@ class ArbiViewController: ARCameraViewController {
     
     override func setupContent() {
         
-        addTrackingNode()
+        addModelNode()
         setupArbiTrack()
     }
     
     
-    func addTrackingNode() {
+    func addModelNode() {
+
+        // モデルのインポート
+        let modelImporter = ARModelImporter(bundled: "bloodhound.armodel")
+        let modelNode = modelImporter?.getNode()
+
+        // モデルのマテリアルを設定
+        let material = ARLightMaterial()
+        material.colour.texture = ARTexture.init(uiImage: UIImage.init(named: "bloodhound.png"))
+        material.diffuse.value = ARVector3.init(valuesX: 0.2, y: 0.2, z: 0.2)
+        material.ambient.value = ARVector3.init(valuesX: 0.8, y: 0.8, z: 0.8)
+        material.specular.value = ARVector3.init(valuesX: 0.3, y: 0.3, z: 0.3)
+        material.shininess = 20
+        material.reflection.reflectivity = 0.15
         
-        // トラッキング(表示)するノードを用意
-        trackingNode = ARImageNode(image: UIImage(named: "CowTracking"))
+        // モデルの ARMeshNode にマテリアルを割り当て
+        if let meshNodes = modelNode?.meshNodes {
+            for case let meshNode as ARMeshNode in meshNodes {
+                meshNode.material = material
+            }
+        }
         
-        // ノードの画像を正しい向きにするために回転
-        trackingNode?.rotate(byDegrees: 90, axisX: 1.0, y: 0.0, z: 0.0)
-        trackingNode?.rotate(byDegrees: 180.0, axisX: 0.0, y: 1.0, z: 0.0)
-        trackingNode?.rotate(byDegrees: 270.0, axisX: 0.0, y: 0.0, z: 1.0)
+        modelNode?.scale(byUniform: 5.0)
+        self.modelNode = modelNode
     }
     
     func setupArbiTrack() {
@@ -63,7 +78,7 @@ class ArbiViewController: ARCameraViewController {
         gyroPlaceManager?.initialise()
         
          // ターゲットとして使うノードを用意
-        let targetImageNode = ARImageNode(image: UIImage(named: "CowTarget"))
+        let targetImageNode = ARImageNode(image: UIImage(named: "target"))
         
         // デバイスのジャイロでノードが動くようにノードを　ARGyroPlaceManager に追加
         gyroPlaceManager?.world.addChild(targetImageNode);
@@ -71,15 +86,14 @@ class ArbiViewController: ARCameraViewController {
         // ノードの画像を正しい向きにするために回転し、サイズを調整
         targetImageNode?.rotate(byDegrees: 90.0, axisX: 1.0, y: 0.0, z: 0.0)
         targetImageNode?.rotate(byDegrees: 180.0, axisX: 0.0, y: 1.0, z: 0.0)
-        targetImageNode?.rotate(byDegrees: 270.0, axisX: 0.0, y: 0.0, z: 1.0)
 
-        targetImageNode?.scale(byUniform: 0.3)
+        targetImageNode?.scale(byUniform: 0.1)
         
         // ARArbiTrack の targetNode に指定
         arbiTrack?.targetNode = targetImageNode
         
-        // ARArbiTracker の world に trackingNode を追加
-        arbiTrack?.world.addChild(trackingNode)
+        // ARArbiTracker の world に modelNode を追加
+        arbiTrack?.world.addChild(modelNode)
     }
     
     override func viewDidLoad() {
